c6f17f78c9ea67bb48e8e44d29a88b760128e37b
Filipe Santos <filipelopesdossantos85@gmail.com>
2025-08-08 08:24:06 -0300

    Erro no pom.xml e criação do meu projeto para JanKenPon

---
+++ src/main/java/flds6/aluno/ifnmg/pedraneles/PedraNeles.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package flds6.aluno.ifnmg.pedraneles;
+
+/**
+ *
+ * @author Filipe
+ */
+public class PedraNeles {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
