279024a9231f489de182abad320c43bcac8fbf51
Filipe Santos <filipelopesdossantos85@gmail.com>
2025-08-08 09:34:21 -0300

    Funcionou e adicionado os metodos abstratos

--- src/main/java/flds6/aluno/ifnmg/pedraneles/PedraNeles.java
+++ src/main/java/flds6/aluno/ifnmg/pedraneles/PedraNeles.java
@@ -4,11 +4,24 @@
 
 package flds6.aluno.ifnmg.pedraneles;
 
+import io.github.guisso.jankenpon.AbstractPlayer;
+import io.github.guisso.jankenpon.Move;
+
 /**
  *
  * @author Filipe
  */
-public class PedraNeles {
+public class PedraNeles extends AbstractPlayer{
+
+    @Override
+    public String getDeveloperName() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Move makeMyMove(Move opponentPreviousMove) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 
    
 }
